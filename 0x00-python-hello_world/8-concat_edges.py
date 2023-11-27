4a646508ccca792e18dbc9876a5de6103653e0ed
Samngase <samuelngase@gmail.com>
2023-11-27 08:01:30 -0800

    Task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-class PrintMessage:
-    def __str__(self):
-        return "object-oriented programming with Python\n"
-
-print(PrintMessage())
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
