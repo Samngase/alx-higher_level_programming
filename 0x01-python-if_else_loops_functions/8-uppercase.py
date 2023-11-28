070d9125bb06a6996e4505849cf5c3a8a8a8e7a7
Samngase <samuelngase@gmail.com>
2023-11-28 09:47:21 -0800

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
-uppercase = _import_('8-uppercase').uppercase
+#!/usr/bin/python3
+def to_upper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
 
-uppercase("holberton")
-uppercase("Holberton School 98 Battery street")
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
